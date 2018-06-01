46e58d72c26b493830b70c725160e163cb03a008
Christopher Arnold <chrisarnold2010@gmail.com>
2018-06-01 20:28:57 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,6 +4,5 @@ function updatePlaylist(playlist,name,song) {
   return playlist;
 }
 function removeFromPlaylist(playlist,name) {
-  delete playlist.name; 
-  return playlist;
+  return delete playlist.name; 
 }
